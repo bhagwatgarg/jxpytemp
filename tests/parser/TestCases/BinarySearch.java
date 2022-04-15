00b7c689c9f9ecfafbd6c52da59ec181c235132e
Rishabh Dugaye <rishabhd@iitk.ac.in>
2022-04-15 13:39:54 +0530

    test cases

--- tests/parser/TestCases/BinarySearch.java
+++ tests/parser/TestCases/BinarySearch.java
@@ -9,15 +9,11 @@ public class BinarySearch{
   boolean a = true;
   // int b = NULL;
 
-<<<<<<< HEAD
-  public static int search(int[] arr, int num){
-=======
   public int search(int[] arr, int num){
->>>>>>> Method Invocation and Overloading working (again)
     int a[] = new int[5];// = {1,2,3,4,5};
     int lef=0, righ=5, mid=0;
     while(lef<=righ){
-      int a = 5;
+      int b = 5;
       mid=lef+(righ-lef)/2;
       if(arr[mid]==num)return mid;
       if(arr[mid]<num)lef=mid+1;
@@ -46,7 +42,295 @@ public class BinarySearch{
 class Main{
 	public static int hello (){
 		BinarySearch a = new BinarySearch();
-		int[] arr = {1,2,3,4,5};
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
+		a.search(arr, 3);
+    return 1;
+	}	
+}package TestCases;
+
+public class BinarySearch{
+
+  int p,q,r,s;
+  // char po, pl;
+  // float x,y,z;
+  double hi;
+  boolean a = true;
+  // int b = NULL;
+
+  public int search(int[] arr, int num){
+    int a[] = new int[5];// = {1,2,3,4,5};
+    int lef=0, righ=5, mid=0;
+    while(lef<=righ){
+      int b = 5;
+      mid=lef+(righ-lef)/2;
+      if(arr[mid]==num)return mid;
+      if(arr[mid]<num)lef=mid+1;
+      else righ=mid-1;
+    }
+    return -1;
+  }
+
+  public static int hello (){
+		int arr = 5;
+		return arr;
+	}	
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+  // public static int hello(int a, int b)
+  // {
+  //   return 1;
+  // }
+
+}
+
+class Main{
+	public static int hello (){
+		BinarySearch a = new BinarySearch();
+		int[] arr = new int[10];
 		a.search(arr, 3);
     return 1;
 	}	
---
+++ tests/parser/TestCases/basic.java
@@ -0,0 +1,10 @@
+class Main{
+    public int hello(int a){
+        return this.hello(1);
+    }
+    public static int main(){
+int[] arr= new int[3];
+arr[1]=3;
+arr[2]=arr[1];
+}
+}
