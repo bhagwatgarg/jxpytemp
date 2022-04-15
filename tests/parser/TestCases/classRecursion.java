46c0171e7de75fee1c9dc97f83ba0518306e748a
Bhagwat Garg <bhagwatgarg10@gmail.com>
2022-04-16 01:25:24 +0530

    testcase update

--- tests/parser/TestCases/classRecursion.java
+++ tests/parser/TestCases/classRecursion.java
@@ -3,10 +3,12 @@
  * 
  */
 
+ // TODO: constructor
+
 package TestCases;
 
 class ListNode {
-  int val;
+  int val2, val;
   ListNode next;
   void ListNode(int b){
     this.next=new ListNode();
@@ -17,15 +19,59 @@ class ListNode {
     int val1;  // should be present in tac
     int val2=this.val;
   }
+
+  void f1(ListNode n1, int c1){
+    int d;
+    return;
+  }
+
+  // int f1(int n1, int c1){
+  //   char d;
+  //   return d;
+  // }
+
+  ListNode f1(int n1, int c1){
+    int n2;
+    n2=1;
+    n1=c1;
+    char d;
+    // ListNode b=new ListNode();
+    return new ListNode();
+  }
 }
 
 class Main{
   public static int main(){
     ListNode root=new ListNode();  // call not present (temp2)
-    ListNode root2=new ListNode(); 
+    // ListNode root2=new ListNode();
     root.next.next.next.val=2;  // .next.next not present
+    int b;
+    root.next.next.val=root.next.next.next.val;  // .next.next not present
     root.next.next.print_val(2); // push root.next.next
-    root2.print_val(3);
+    // root.print_val(3);
+
+    // int n1;
+    ListNode l2=new ListNode();
+    root=l2;
+    int a=1;
+    a=b;
+    root.f1(root, 2);
+    root.f1(a, 2);
+
+    for(int i=0; i<10; i++){
+      
+      a=b;
+    }
+
+    while(a == 1)
+    {
+      a = b;
+    }
+
+    do{
+      a = b;
+    }while(a == 1);
     return 1;
+// TODO: do while extra goto
   }
 }
