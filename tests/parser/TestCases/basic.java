0c0a999a0f149d1ce1afbadf6f87d8e0555b0d1d
Bhagwat Garg <bhagwatgarg10@gmail.com>
2022-04-25 05:40:40 +0530

    added tests

--- tests/parser/TestCases/basic.java
+++ tests/parser/TestCases/basic.java
@@ -36,7 +36,7 @@ class Main{
     int a =5, b_ = 6, c = 7;
     a=5;
     // int i;
-    // for(int i=0; i <10; i=i+1){
+    // for(int i=0; i <10; i=i+1){git checkout 
     // while(a<10){
     //   a=a+1;
     //   for(int i = 0; i < 10; i = i + 1)
@@ -46,18 +46,27 @@ class Main{
     // }
     // imp.print_int(a);
     // imp.print_int(c);
-    int b= this.fact(5, 3, 10);
+    int b=0;
+    
     // int b = -5;
     // b += 2;
-    imp.print_int(b);
+    // imp.print_int(b);
     // for(int i=0; i<100; i++){
     //   imp.print_int(i);
     // }
+    a=98;
     // int arr[]=new int[30];
-    // b=arr[2];
-    // int b=5;
-    // imp.print_int(b);
-    // imp.print_int(b);
-    return 0;
+    // for (int i=0;i<10;i++){
+    //   arr[i]=i;
+    //   imp.print_int(arr[i]);
+    // }
+
+    int arr[][] = new int[5][3];
+
+    return a;
   }
 }
+
+/*
+mov [rbp+8], qword [rbp+16]
+*/
